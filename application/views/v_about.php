e8aff3dca6de1eb5884d8668838f6bab131ec303
halawa_oji97 <ozidhalawa97@gmail.com>
2021-10-18 21:09:33 +0700

    mengerjakan latihan pertemuan 6

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Oji</li>
+        <li>Nama Belakang : Halawa</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Kelapa Gading, Jakarta Utara</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Nias, Medan, Sumatera Utara</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
